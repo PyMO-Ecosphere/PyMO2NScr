5cfe9c02651c3098dd912d36325cae02cb087aab
许兴逸 <853974536@qq.com>
2024-03-01 13:37:24 +0800

    saveResult

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import IDAllocator
+import Text.Builder
+import Data.Text (unpack)
+import Data.Encoding (Encoding, encodeLazyByteString)
+import Data.Encoding.GB18030 (GB18030 (GB18030))
+import qualified Data.ByteString.Lazy as BS
+import Data.Bits (xor)
+import System.FilePath ((</>))
+
+
+saveResult :: Encoding enc => enc -> Builder -> Bool -> FilePath -> IO ()
+saveResult enc result encrypted outDir =
+  let bs = encodeLazyByteString enc $ unpack $ run result in
+  if encrypted
+    then BS.writeFile (outDir </> "nscript.dat") $ BS.map (xor 0x84) bs
+    else BS.writeFile (outDir </> "0.txt") bs
+
 
 main :: IO ()
 main = do
+  saveResult GB18030 "测试" False "./"
   putStrLn "hello world"
