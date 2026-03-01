c2dbaefd667f7b5f6594092e20470d86742e0861
许兴逸 <853974536@qq.com>
2026-03-01 14:29:59 +0800

    完善Compiler

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -6,15 +6,18 @@ module Compiler
   , makeCompilerInput
   , runCompiler ) where
 
-import Control.Monad.RWS (RWST, runRWST)
+import Control.Monad.RWS (RWST, runRWST, get, put, modify, gets, asks)
+import qualified Control.Monad.RWS as RWS
 import qualified TextBuilder (TextBuilder, toText)
 import qualified Language.PyMO.GameConfig as PyMO
+import qualified Language.PyMO.Script as PyMO
 import qualified Data.Text as T
 import qualified Data.HashMap.Strict as HM
 import qualified Data.Text.Encoding as T
 import Data.FileEmbed (embedFile)
 import System.FilePath ((</>))
 
+-- Compiler Input
 data CompilerInput = CompilerInput
   { ciPyMOGameConfig :: PyMO.GameConfig
   , ciPyMOGameDir :: FilePath }
@@ -24,13 +27,53 @@ makeCompilerInput gameDir = do
   gameConfig <- PyMO.loadGameConfig $ gameDir </> "gameconfig.txt"
   return $ CompilerInput gameConfig gameDir
 
+getCompilerInput :: (CompilerInput -> a) -> Compiler a
+getCompilerInput = Compiler . asks
+
+-- Compiler State
+type VarName = T.Text
+type VarId = Int
+type ScriptName = T.Text
+type ScriptId = Int
+
 data CompilerState = CompilerState
-  { csVariables :: HM.HashMap T.Text Int }
+  { csVariables :: HM.HashMap VarName VarId
+  , csLoadedScripts :: HM.HashMap ScriptName (ScriptId, PyMO.Script) }
 
 emptyCompilerState :: CompilerState
 emptyCompilerState = CompilerState
   { csVariables = HM.empty }
 
+updateCompilerState :: (CompilerState -> CompilerState) -> Compiler ()
+updateCompilerState = Compiler . modify
+
+getCompilerState :: (CompilerState -> a) -> Compiler a
+getCompilerState = Compiler . gets
+
+getVarId :: VarName -> Compiler VarId
+getVarId varName = Compiler $ do
+  state <- get
+  case HM.lookup varName (csVariables state) of
+    Just varId -> return varId
+    Nothing -> do
+      let newId = HM.size (csVariables state)
+      put $ state { csVariables = HM.insert varName newId (csVariables state) }
+      return newId
+
+loadPyMOScript :: ScriptName -> Compiler (ScriptId, PyMO.Script)
+loadPyMOScript scriptName = do
+  loadedScripts <- getCompilerState csLoadedScripts
+  case HM.lookup scriptName loadedScripts of
+    Just x -> return x
+    Nothing -> do
+      gameDir <- getCompilerInput ciPyMOGameDir
+      script <- liftIO $ PyMO.loadPyMOScript gameDir (T.unpack scriptName)
+      let scriptId = HM.size loadedScripts
+          loadedScripts' = HM.insert scriptName (scriptId, script) loadedScripts
+      updateCompilerState $ \x -> x { csLoadedScripts =  loadedScripts' }
+      return (scriptId, script)
+
+-- Compiler Output
 type Hole = TextBuilder.TextBuilder
 
 data CompilerOutput = CompilerOutput
@@ -38,6 +81,18 @@ data CompilerOutput = CompilerOutput
   , coDefines :: Hole
   , coBody :: Hole }
 
+instance Semigroup CompilerOutput where
+  a <> b = CompilerOutput
+    { coHeader = coHeader a <> coHeader b
+    , coDefines = coDefines a <> coDefines b
+    , coBody = coBody a <> coBody b
+    }
+
+instance Monoid CompilerOutput where
+  mempty = CompilerOutput mempty mempty mempty
+  mappend = (<>)
+
+-- Compiler
 newtype Compiler x =
   Compiler (RWST CompilerInput CompilerOutput CompilerState IO x)
 
@@ -52,16 +107,8 @@ instance Monad Compiler where
   return = pure
   Compiler m >>= f = Compiler (m >>= (\x -> let Compiler r = f x in r))
 
-instance Semigroup CompilerOutput where
-  a <> b = CompilerOutput
-    { coHeader = coHeader a <> coHeader b
-    , coDefines = coDefines a <> coDefines b
-    , coBody = coBody a <> coBody b
-    }
-
-instance Monoid CompilerOutput where
-  mempty = CompilerOutput mempty mempty mempty
-  mappend = (<>)
+liftIO :: IO a -> Compiler a
+liftIO = Compiler . RWS.liftIO
 
 runCompiler :: CompilerInput -> Compiler () -> IO T.Text
 runCompiler ci (Compiler compiler) = do
@@ -70,9 +117,9 @@ runCompiler ci (Compiler compiler) = do
   where
     nscrTemplate = T.lines $ T.decodeUtf8 $(embedFile "./src/nscr-template.txt")
     holeMapping =
-      [ ("header", coHeader)
-      , ("defines", coDefines)
-      , ("body", coBody) ]
+      [ (T.pack "header", coHeader)
+      , (T.pack "defines", coDefines)
+      , (T.pack "body", coBody) ]
     applyHole co line =
       case T.stripPrefix "&&" line of
         Nothing -> line
